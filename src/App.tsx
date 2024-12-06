78b08ba105ac3c3a7479ad9944cc24cc723af875
thus-artistry <ametepemalthus16@gmail.com>
2024-12-06 17:45:38 +0100

    page contact concue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Contact from './pages/contact/Contact';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Contact/>
     </div>
   );
 }
